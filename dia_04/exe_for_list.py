c24c4e524fe7a3ea419428e3ac741bd0b2f40865
fdg-fer <fdiasguima@gmail.com>
2025-03-31 20:45:35 -0300

    novos exercios

---
+++ dia_04/exe_for_list.py
@@ -0,0 +1,13 @@
+# Escreva um programa que receba uma lista de números
+# do usuario e conte quantas vezes um número
+# específico aparece na lista
+# solicite ao usuário um numero e exiba a contagem
+
+lista = [5,4,4,8,8,7,8,9,6,1,7,2,3,6,0,0,1,5,4]
+numero = int(input("Digite um numero:"))
+contador = 0
+
+for x in lista:
+    if x == numero:
+        contador+=1
+print(contador)
